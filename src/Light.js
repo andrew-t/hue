773d495da16298bb74c103df77977e090f00c239
Andrew <gitkraken@andrewt.net>
2018-06-27 09:23:10 +0100

    tidying; cancel on set

--- src/Light.js
+++ src/Light.js
@@ -13,29 +13,34 @@ class Light extends Lightable {
 	}
 
 	set(payload) {
+		this.cancelAuto();
 		return this.luxcaster.http.put(`/lights/${this.id}/state`, payload);
 	}
 
 	autoOn() {
 		const state = states.appropriateState(),
-			checkAndUpdateSoon = () => {
-				setTimeout(() => {
-					this.getState().then(actual => {
-						if (actual.on == state.on &&
-							actual.bri == state.bri &&
-							actual.hue == state.hue &&
-							actual.sat == state.sat)
-								return this.autoOn();
-						else console.log('Cancelling auto for', this.name, state, actual);
-					})
-					.catch(error => {
-						console.error('Error updating auto light', this.name, error);
-						checkAndUpdateSoon();
-					});
-				}, this.luxcaster.config.autoInterval || 60000);
-			};
+			promise = this.set(state),
+			checkAndUpdateSoon = () => this._autoTimeout = setTimeout(() =>
+				this.getState().then(actual => {
+					if (actual.on == state.on &&
+						actual.bri == state.bri &&
+						actual.hue == state.hue &&
+						actual.sat == state.sat)
+							return this.autoOn();
+					else console.log('Cancelling auto for', this.name, state, actual);
+				}).catch(error => {
+					console.error('Error updating auto light', this.name, error);
+					checkAndUpdateSoon();
+				}),
+				this.luxcaster.config.autoInterval || 60000);
 		checkAndUpdateSoon();
-		return this.luxcaster.http.put(`/lights/${this.id}/state`, state);
+		return promise;
+	}
+	cancelAuto() {
+		if (this._autoTimeout) {
+			clearTimeout(this._autoTimeout);
+			this._autoTimeout = null;
+		}
 	}
 }
 
